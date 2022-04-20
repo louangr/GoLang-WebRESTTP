b5cba7bf502cf0d6ecc64eba13d20c87dd127ff7
louan <louan.geray@ucaya.com>
2022-04-20 21:47:55 +0200

    updtate start.cmd to clean .db and .db.lock files

--- commands/start.cmd
+++ commands/start.cmd
@@ -1,6 +1,8 @@
 SET portNumber=8000
 SET dbFileName="myBolt"
 
+DEL %dbFileName%.db
+DEL %dbFileName%.db.lock
 go clean -cache ./cmd/restserver/...
 go build -o ./cmd/restserver -v ./cmd/restserver/...
 .\cmd\restserver\restserver.exe %portNumber% %dbFileName%
